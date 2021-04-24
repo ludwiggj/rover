ebc1d224cca3726b8853402d88ba9c447e4ae699
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2021-04-24 12:51:51 +0100

    Can move off grid and reappear

--- src/main/scala/com/ludwig/rover/Mover.scala
+++ src/main/scala/com/ludwig/rover/Mover.scala
@@ -9,7 +9,7 @@ trait Mover {
   private val directions: List[Direction] = List(Up, Left, Down, Right)
   private val numberOfDirections = directions.size
 
-  def move(m: MoveInstruction, p: Position, d: Direction): (Position, Direction) = {
+  def move(m: MoveInstruction, p: Position, d: Direction, gridTopRight: Position): (Position, Direction) = {
     m match {
       case RotateClockwise90 =>
         val newDirection = directions((directions.indexOf(d) + 1) % numberOfDirections)
@@ -22,10 +22,18 @@ trait Mover {
 
       case Forward =>
         d match {
-          case Up => (p.copy(y = p.y + 1), d)
-          case Left => (p.copy(x = p.x - 1), d)
-          case Down => (p.copy(y = p.y - 1), d)
-          case Right => (p.copy(x = p.x + 1), d)
+          case Up =>
+            val newY = if (p.y == gridTopRight.y) 0 else p.y + 1
+            (p.copy(y = newY), d)
+          case Left =>
+            val newX = if (p.x == 0) gridTopRight.x else p.x - 1
+            (p.copy(x = newX), d)
+          case Down =>
+            val newY = if (p.y == 0) gridTopRight.y else p.y - 1
+            (p.copy(y = newY), d)
+          case Right =>
+            val newX = if (p.x == gridTopRight.x) 0 else p.x + 1
+            (p.copy(x = newX), d)
         }
     }
   }
--- src/test/scala/com/ludwig/rover/MoverTest.scala
+++ src/test/scala/com/ludwig/rover/MoverTest.scala
@@ -5,64 +5,85 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class MoverTest extends AnyFlatSpec with should.Matchers with BeforeAndAfterAll {
+  val fiveByFiveGrid = Position(4, 4)
 
   "turnClockwise90" should "maintain position and change direction from Left to Down" in {
     object Sut extends Mover
-    Sut.move(RotateClockwise90, Position(1,1), Left) should equal(Position(1,1), Down)
+    Sut.move(RotateClockwise90, Position(1,1), Left, fiveByFiveGrid) should equal(Position(1,1), Down)
   }
 
   it should "maintain position and change direction from Down to Right" in {
     object Sut extends Mover
-    Sut.move(RotateClockwise90, Position(1,1), Down) should equal(Position(1,1), Right)
+    Sut.move(RotateClockwise90, Position(1,1), Down, fiveByFiveGrid) should equal(Position(1,1), Right)
   }
 
   it should "maintain position and change direction from Right to Up" in {
     object Sut extends Mover
-    Sut.move(RotateClockwise90, Position(1,1), Right) should equal(Position(1,1), Up)
+    Sut.move(RotateClockwise90, Position(1,1), Right, fiveByFiveGrid) should equal(Position(1,1), Up)
   }
 
   it should "maintain position and change direction from Up to Left" in {
     object Sut extends Mover
-    Sut.move(RotateClockwise90, Position(1,1), Up) should equal(Position(1,1), Left)
+    Sut.move(RotateClockwise90, Position(1,1), Up, fiveByFiveGrid) should equal(Position(1,1), Left)
   }
 
   "turnAnticlockwise90" should "maintain position and change direction from Left to Up" in {
     object Sut extends Mover
-    Sut.move(RotateAntiClockwise90, Position(1,1), Left) should equal(Position(1,1), Up)
+    Sut.move(RotateAntiClockwise90, Position(1,1), Left, fiveByFiveGrid) should equal(Position(1,1), Up)
   }
 
   it should "maintain position and change direction from Up to Right" in {
     object Sut extends Mover
-    Sut.move(RotateAntiClockwise90, Position(1,1), Up) should equal(Position(1,1), Right)
+    Sut.move(RotateAntiClockwise90, Position(1,1), Up, fiveByFiveGrid) should equal(Position(1,1), Right)
   }
 
   it should "maintain position and change direction from Right to Down" in {
     object Sut extends Mover
-    Sut.move(RotateAntiClockwise90, Position(1,1), Right) should equal(Position(1,1), Down)
+    Sut.move(RotateAntiClockwise90, Position(1,1), Right, fiveByFiveGrid) should equal(Position(1,1), Down)
   }
 
   it should "maintain position and change direction from Down to Left" in {
     object Sut extends Mover
-    Sut.move(RotateAntiClockwise90, Position(1,1), Down) should equal(Position(1,1), Left)
+    Sut.move(RotateAntiClockwise90, Position(1,1), Down, fiveByFiveGrid) should equal(Position(1,1), Left)
   }
 
   "move forward" should "move up if facing up" in {
     object Sut extends Mover
-    Sut.move(Forward, Position(1,1), Up) should equal(Position(1,2), Up)
+    Sut.move(Forward, Position(1,1), Up, fiveByFiveGrid) should equal(Position(1,2), Up)
   }
 
   it should "move left if facing left" in {
     object Sut extends Mover
-    Sut.move(Forward, Position(1,1), Left) should equal(Position(0,1), Left)
+    Sut.move(Forward, Position(1,1), Left, fiveByFiveGrid) should equal(Position(0,1), Left)
   }
 
   it should "move down if facing down" in {
     object Sut extends Mover
-    Sut.move(Forward, Position(1,1), Down) should equal(Position(1,0), Down)
+    Sut.move(Forward, Position(1,1), Down, fiveByFiveGrid) should equal(Position(1,0), Down)
   }
 
   it should "move right if facing right" in {
     object Sut extends Mover
-    Sut.move(Forward, Position(1,1), Right) should equal(Position(2,1), Right)
+    Sut.move(Forward, Position(1,1), Right, fiveByFiveGrid) should equal(Position(2,1), Right)
+  }
+
+  it should "reappear at bottom if move up at top" in {
+    object Sut extends Mover
+    Sut.move(Forward, Position(1,4), Up, fiveByFiveGrid) should equal(Position(1,0), Up)
+  }
+
+  it should "reappear at right if move left at left edge" in {
+    object Sut extends Mover
+    Sut.move(Forward, Position(0,2), Left, fiveByFiveGrid) should equal(Position(4,2), Left)
+  }
+
+  it should "reappear at top if move down at bottom" in {
+    object Sut extends Mover
+    Sut.move(Forward, Position(1,0), Down, fiveByFiveGrid) should equal(Position(1,4), Down)
+  }
+
+  it should "reappear at left if move right at right edge" in {
+    object Sut extends Mover
+    Sut.move(Forward, Position(4,2), Right, fiveByFiveGrid) should equal(Position(0,2), Right)
   }
 }
